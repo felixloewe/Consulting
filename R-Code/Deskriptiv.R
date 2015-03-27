7e08a6fcd594f249ba12ae3b36e50ba3dbdaab52
uaccc <uaccc2@gmail.com>
2015-03-27 16:22:12 +0100

    Kontinente hinzugefügt. Liegen jetzt als Edge-Attribute vor:
    E(Gr)$continents_exp # Export-Kontinente
    E(Gr)$continents_imp # Import-Kontinent

--- R-Code/Deskriptiv.R
+++ R-Code/Deskriptiv.R
@@ -231,3 +231,34 @@ Gr1992 <- GraphYear[[1]]
 Gr1992$
 =======
 >>>>>>> e28141a73c85b15e1bafec6f07cb0edd633d4066
+
+
+
+
+
+## Kontinente hinzufügen
+
+# Countrycode-Paket laden
+install.packages("countrycode")
+library(countrycode)
+
+# Kontinente hinzufügen. Format des Ursprungscodes: CoW-Numeric = Correlates of War Numeric
+# Correlates of War versucht, wissenschaftlich standardisierte Daten über Kriege zu sammeln und
+# zu Verfügung zu stellen.
+MADdata$continents_exp <- countrycode(MADdata$Reporter_Code, "cown", "continent", warn = TRUE)
+MADdata$continents_imp <- countrycode(MADdata$Partner_Code, "cown", "continent", warn = TRUE)
+
+# Herausfinden, welche Länder der Kontinente nicht gematcht werden können.
+# Sind keinem Kontinent zugeordnet (Sonderverwaltungszonen).
+unique(
+  MADdata[
+    MADdata$Reporter_Code %in%
+      c(10, 11, 30, 65, 66, 68, 120, 231, 315, 345, 348, 585, 713, 720, 721, 815, 925, 930, 960, 
+        980, 991, 995, 1029, 1032, 1065, 1069, 1073, 1163, 4050, 5812, 9002, 9004, 9201, 9321),
+    c("Reporter_Name", "Reporter_Code")
+    ]
+)
+
+# So liegen die Kontinente jetzt vor (Edge-Attribute).
+E(Gr)$continents_exp # Export-Kontinente
+E(Gr)$continents_imp # Import-Kontinente
